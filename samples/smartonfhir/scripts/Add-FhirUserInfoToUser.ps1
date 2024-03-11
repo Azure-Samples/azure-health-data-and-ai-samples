dd2e8cf845c83fe77aecb2a8e03fcee393cf40ff
Shreshtha Gupta <v-shresgupta@microsoft.com>
2024-03-11 13:05:46 +0530

    Updated Script and Inferno doc

--- samples/smartonfhir/scripts/Add-FhirUserInfoToUser.ps1
+++ samples/smartonfhir/scripts/Add-FhirUserInfoToUser.ps1
@@ -1,3 +1,6 @@
+<#
+    Populates a fhirUser Directory Extension for a particular user with a particular value.
+#>
 param (
     [Parameter(Mandatory=$true)]
     [string]$ApplicationId,
@@ -8,6 +11,39 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$FhirUserValue
 )
+
+if ([string]::IsNullOrWhiteSpace($ApplicationId)) {
+
+    Write-Host "ApplicationId parameter blank, looking in azd enviornment configuration...."
+
+    # Load parameters from active Azure Developer CLI environment
+    $AZD_ENVIRONMENT = $(azd env get-values --cwd $SAMPLE_ROOT)
+    $AZD_ENVIRONMENT | foreach {
+        $name, $value = $_.split('=')
+        if ([string]::IsNullOrWhiteSpace($name) -or $name.Contains('#')) {
+            continue
+        }
+        
+        if ([string]::IsNullOrWhiteSpace($ApplicationId) -and $name -eq "ApplicationId") {
+            $FhirResourceAppId = $value.Trim('"')
+        }
+    }
+}
+
+if (-not $ApplicationId) {
+    Write-Error "ApplicationId is STILL not set. Exiting."
+    exit
+}
+
+if (-not $UserObjectId) {
+    Write-Error "UserObjectId is not set. Exiting."
+    exit
+}
+
+if (-not $FhirUserValue) {
+    Write-Error "FhirUserValue is not set. Exiting."
+    exit
+}
 
 $appIdFormatted = $ApplicationId.Replace("-", "")
 
