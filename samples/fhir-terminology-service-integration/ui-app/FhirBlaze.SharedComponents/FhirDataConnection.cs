954df77d9a2454a790b5f1edbabd3de32532b3c2
ms-snarang <shashank.n@waferwire.com>
2025-11-13 22:01:06 +0530

    TS and lift shift sample removal (#364)

--- samples/fhir-terminology-service-integration/ui-app/FhirBlaze.SharedComponents/FhirDataConnection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FhirBlaze.SharedComponents
-{
-    public class FhirDataConnection
-    {
-        public string FhirServerUri { get; set; }
-        public string Authority { get; set; }
-        public string Scope { get; set; }
-    }
-}
--- samples/fhir-terminology-service-integration/ui-app/FhirBlaze/Model/CodeSystemModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FhirBlaze.Model
-{
-    public class CodeSystemModel
-    {
-       
-        public string CodeSystemName { get; set; }
-        public string CodeSystemValue { get; set; }
-    }
-}
